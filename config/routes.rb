f8578e1c778594f2ad859780a6c61eb4430a91d7
EC2 Default User <ec2-user@ip-172-31-38-245.ap-northeast-1.compute.internal>
2023-09-03 07:30:54 +0000

    “応用課題4”

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'favorites/create'
-  get 'favorites/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
